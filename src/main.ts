a027f4511e748a1eb2b9dc9d783ecd4c7a03fc72
mallowigi <heliosaian@gmail.com>
2019-11-29 22:46:27 +0200

    init protobuf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule }   from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Transport }      from '@nestjs/common/enums/transport.enum';
+import { NestFactory }    from '@nestjs/core';
+import { join }           from 'path';
+import { AppModule }      from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options:   {
+      package:   'service',
+      url:       '0.0.0.0:50050',
+      protoPath: join(__dirname, '../../common/proto/users/service.proto'),
+    },
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform:            true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  // tslint:disable-next-line:no-console
+  await app.listen(() => console.log('users microservice is listening'));
 }
 
 bootstrap();
